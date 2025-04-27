517416dc70010cced99a3004d8f4637120f114fd
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-27 17:16:53 +0200

    Added the product table

--- ecommerce.sql
+++ ecommerce.sql
@@ -14,3 +14,19 @@ CREATE TABLE brand (
     brand_id INT PRIMARY KEY AUTO_INCREMENT,
     brand_name VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE product (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    brand_id INT,
+    category_id INT,
+    base_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (brand_id) REFERENCES brand(brand_id),
+    FOREIGN KEY (category_id) REFERENCES product_category(category_id)
+
+CREATE TABLE product_image (
+    image_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    image_url VARCHAR(500),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
