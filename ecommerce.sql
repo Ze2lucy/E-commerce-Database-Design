599825ba88b26ba3500449395d46ea83a4a35960
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-27 16:57:54 +0200

    created CATEGORY TABLES

--- ecommerce.sql
+++ ecommerce.sql
@@ -2,3 +2,15 @@
 -- Group members that Participate: [Zethu Sithole]
 -- Group Project: E-commerce Database
 -- Description: Creates schema for e-commerce platform
+
+-- CATEGORY TABLES
+
+CREATE TABLE product_category (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE brand (
+    brand_id INT PRIMARY KEY AUTO_INCREMENT,
+    brand_name VARCHAR(100) NOT NULL
+);
